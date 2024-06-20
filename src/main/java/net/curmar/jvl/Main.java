ccda690c88a7bd4cfe20ca92dfd5374109092b91
AlexR <cursedmaria@mail.ru>
2024-06-21 02:41:26 +0700

    First with enum.

--- src/main/java/net/curmar/jvl/Main.java
+++ src/main/java/net/curmar/jvl/Main.java
@@ -2,38 +2,20 @@ package net.curmar.jvl;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.EnumMap;
 
-class EnemyComparator implements Comparator<Enemy> {
+enum Enemies {
+    EASY(0), MED(1), HARD(5);
+    public final int level;
 
-    public static final EnemyComparator INSTANCE = new EnemyComparator();
-
-    @Override
-    public int compare(Enemy a, Enemy b) {
-        return (100 * a.damage + 10 * a.health + a.protection) - (100 * b.damage + 10 * b.health + b.protection);
+    Enemies(int l) {
+        level = l;
     }
 }
-
-class Enemy {
-
-    public int damage, health, protection;
-
-    Enemy(int a, int b, int c) {
-        damage = a;
-        health = b;
-        protection = c;
-    }
-}
-
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Enemy> a = new ArrayList<>();
-        a.add(new Enemy(5, 3, 1));
-        a.add(new Enemy(9, 9, 1));
-        a.add(new Enemy(5, 2, 1));
-        a.sort(EnemyComparator.INSTANCE);
-        for (Enemy e : a)
-            System.out.println(e.health);
+
     }
 }
